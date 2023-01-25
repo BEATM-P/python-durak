03f818c1a1a853cc3d2c4179e93253b94d344b32
BEATM_P <pesimei@gmail.com>
2023-01-26 00:07:59 +0100

    modified:   logic.py     nearly finished game logic     started on player logic, mb players need own file :/

--- logic.py
+++ logic.py
@@ -1,15 +1,22 @@
 import random
+from window import playerdisplay
 cards=['H1','H2','H3','H4', 'H5', 'H6', 'H7', 'H8','H9,','H10', 'H11', 'H12', 'H13', 'H14']
 
 
 
-class player():
+class player(): #somehow distinguish remote and local players 
     def __init__(self) -> None:
-        cards=[]
+        self.cards=[]
 
-    def take(card):
-        cards.insert(card)
+    def take(self,card):
+        self.cards.append(card)
 
+    def attack(self, table):
+        return playerdisplay.getAttack(self.cards, table)
+    
+    def defend(self, table):
+        return playerdisplay.getDefense(self.cards,table)
+    
 
 class game:
     def __init__(self) -> None:
@@ -33,24 +40,40 @@ class game:
         for i in range(6):
             for i in self.players:
                 i.take(self.curr_deck.pop())
+        self.trump=self.curr_deck[0]
 
     def deal_one(self):
         if self.curr_deck !=[]:
             return self.curr_deck.pop()
 
     def game(self):
-        playernum=0
+        playernum=1                 #determines which player gets attacked TODO Find lowest trump card
+        self.table=[]
         while len(self.players)>1:
-            self.play(self.players[1],self.players[2], self.players[1], [])
+            playernum+=self.play(self.players[playernum-1],self.players[playernum], self.players[playernum+1], self.table)  
+            if self.table==[]:              #if table is not empty attack is still going(schiebung)
+                for i in self.players:
+                    i.take_stack(6-len(i.cards))
+                    if i.isDone():
+                        self.players.remove(i)              #if there are no more cards and a player has no cards hes finished
+            playernum % len(self.players)
+        
 
-    def play(self,p1, p2, p3):
-        table=p1.attack([])
-        if p2.schiebt():
 
-        while True:
-            table+=p1.attack(table)
-            table=p2.defend(table)
-            table+=p3.attack(table)
+    def play(self,p1, p2, p3):                      #return 0 if defending player wins, otherwise 1
+        self.table=p1.attack([])
+        if p2.schiebt():                            #dont change table, but next player will be attacked
+            return 1
+        self.table+=p2.attack(self.table)   
+        while p2.defend() and len(self.table)<len(p2.cards):
+            self.table+=p1.attack(self.table)
+            self.table+=p3.attack(self.table)
+        p2.defend()
+        if self.table==[]:  
+            return 0                    #-> p2 has won-> return 0
+        else:
+            p2.take(self.table)
+            return 1 
 
 
     
